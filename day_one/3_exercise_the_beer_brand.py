101dc0ecd60537a3ea82d1ee51361521032150a1
oliverjunior1 <oliver-junior@hotmail.com>
2025-12-27 16:49:25 -0300

    Create 3_exercise_the_beer_brand.py

---
+++ day_one/3_exercise_the_beer_brand.py
@@ -0,0 +1,4 @@
+name_pet = input("What's the name of your first pet? ")
+name_girlfriend = input("Whats is the name of your first girlfriend? ")
+
+print(f"The beer brand is '{name_pet} {name_girlfriend}'")
