52b5953c9b4003cfebd8dde2598d190ec82169c5
oliverjunior1 <oliver-junior@hotmail.com>
2025-12-27 17:13:41 -0300

    Delete 3_exercise_the_beer_brand.py

--- day_one/3_exercise_the_beer_brand.py
+++
@@ -1,4 +0,0 @@
-name_pet = input("What's the name of your first pet? ")
-name_girlfriend = input("Whats is the name of your first girlfriend? ")
-
-print(f"The beer brand is '{name_pet} {name_girlfriend}'")
