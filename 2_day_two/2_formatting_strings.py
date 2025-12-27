6d8b6bf110fa1bc4f7f94a52ef6b606a269cdca2
oliverjunior1 <oliver-junior@hotmail.com>
2025-12-27 18:46:53 -0300

    Update 2_formatting_strings.py

--- 2_day_two/2_formatting_strings.py
+++ 2_day_two/2_formatting_strings.py
@@ -1,5 +1,6 @@
 name = "Joaquim"
 age = 30
+age_plus = 1
 
-print("My name is {} and my age is {} years old.".format(name, age))
+print("My name is {} and my age is {} years old and I will have {} years old in the next year.".format(name, age, age+age_plus))
 print(f"My name is {name} and my age is {age} years old.")
