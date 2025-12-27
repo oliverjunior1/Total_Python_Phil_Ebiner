750d6904ef3983d59f2ac5da1ebe00aa321b03fd
oliverjunior1 <oliver-junior@hotmail.com>
2025-12-27 17:13:37 -0300

    Delete 2_input.py

--- day_one/2_input.py
+++
@@ -1,2 +0,0 @@
-# print("Your name is " + input("Tell me your name: ") + " and your last name is " + input("Tell me your last name: "))
-print(input("What's your name?") + " " + input("What's your surname?"))
