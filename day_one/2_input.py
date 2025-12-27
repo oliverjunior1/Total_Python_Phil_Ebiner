93dbcfc798862896af7c5b8a70785b4f3f45ba01
oliverjunior1 <oliver-junior@hotmail.com>
2025-12-26 23:41:10 -0300

    Update 2_input.py

--- day_one/2_input.py
+++ day_one/2_input.py
@@ -1,1 +1,2 @@
-print("Your name is " + input("Tell me your name: ") + " and your last name is " + input("Tell me your last name: "))
+# print("Your name is " + input("Tell me your name: ") + " and your last name is " + input("Tell me your last name: "))
+print(input("What's your name?") + " " + input("What's your surname?"))
