8b7a8b95e1ee6c9a35039553c64b38c7cc15d8cd
oliverjunior1 <oliver-junior@hotmail.com>
2025-12-28 17:12:14 -0300

    Create 2_slice.py

---
+++ 3_day_three/2_slice.py
@@ -0,0 +1,16 @@
+text = "ABCDEFGHIJKLM"
+fragment = text[2:]
+fragment2 = text[:5]
+fragment3 = text[2:10:2]
+fragment4 = text[::3]
+fragment5 = text[::-1]
+fragment6 = text[::-2]
+
+
+
+print(fragment)
+print(fragment2)
+print(fragment3)
+print(fragment4)
+print(fragment5)
+print(fragment6)
