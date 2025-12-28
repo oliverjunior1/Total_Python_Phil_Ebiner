72497715813f71b035d60a6b1d475d4a06fe850b
oliverjunior1 <oliver-junior@hotmail.com>
2025-12-28 17:27:31 -0300

    Create 2_slice_strings.py

---
+++ exercises/2_slice_strings.py
@@ -0,0 +1,4 @@
+# Take every third character starting from the ninth to the end of the sentence, and print the result.
+sentence = "Controlling complexity is the essence of programming"
+
+print(sentence[8::3])
