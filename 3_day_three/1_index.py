c855932e99d7ad5bb39b20a0ed8fc62b6566b693
oliverjunior1 <oliver-junior@hotmail.com>
2025-12-28 16:20:20 -0300

    Create 1_index.py

---
+++ 3_day_three/1_index.py
@@ -0,0 +1,7 @@
+my_text = "This text is a test"
+
+# This numbers represents the start and the end of the text to be considered.
+result = my_text.index('s',5,12)
+result2 = my_text.rindex('s')
+print(result)
+print(result2)
