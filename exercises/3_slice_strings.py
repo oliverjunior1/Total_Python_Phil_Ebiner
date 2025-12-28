251f3d323a0d47c44c2184811c6ca676015913ca
oliverjunior1 <oliver-junior@hotmail.com>
2025-12-28 17:52:26 -0300

    Create 3_slice_strings.py

---
+++ exercises/3_slice_strings.py
@@ -0,0 +1,4 @@
+# Reverses the position of all the characters in the following sentence and displays the result on the screen.
+sentence = "It's great to work with computers. They don't argue, they remember everything and they don't drink your beer"
+
+print(sentence[::-1])
