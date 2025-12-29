11e4bb80998b478962b37e848808488d0425d656
oliverjunior1 <oliver-junior@hotmail.com>
2025-12-29 19:54:50 -0300

    Create 4_string_methods.py

---
+++ exercises/4_string_methods.py
@@ -0,0 +1,6 @@
+# Join the following list into a string, separating each item with a space.
+# Use the appropriate list/string method, and display the result.
+
+word_list = ["Simple","is","better","than","complex."]
+
+print(" ".join(word_list))
