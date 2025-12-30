25d7fa8d9085b56dc28ca0945713416b00ffd168
oliverjunior1 <oliver-junior@hotmail.com>
2025-12-30 19:06:24 -0300

    Create 8_tuples.py

---
+++ exercises/8_tuples.py
@@ -0,0 +1,6 @@
+# Use a tuple method to count the number of times the value 2 appears in the following tuple,
+# and display the result (integer) on the screen:
+
+my_tuple = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
+
+print(my_tuple.count(2))
