c37591632f80d05147749e1df7811ce122f623cf
oliverjunior1 <oliver-junior@hotmail.com>
2025-12-30 19:56:22 -0300

    Create 9_bool.py

---
+++ 3_day_three/9_bool.py
@@ -0,0 +1,21 @@
+var1 = True
+var2 = False
+print(type(var1))
+print(var1)
+
+number = 5 > 2 + 3
+print(type(number))
+print(number)
+number = 5 == 2+3
+print(number)
+
+number = 5 >= 2 + 3
+print(number)
+
+number = 5 != 2 + 3
+print(number)
+
+list = [1,2,3,4]
+control = 5 in list
+print(type(control))
+print(control)
