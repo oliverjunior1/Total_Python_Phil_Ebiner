66b6e7f3b96a45f78b9f7c2ca95f04dad8f523b5
oliverjunior1 <oliver-junior@hotmail.com>
2025-12-29 21:17:58 -0300

    Create 6_dict.py

---
+++ exercises/6_dict.py
@@ -0,0 +1,4 @@
+# Use print to returns the second item of the list called points2, inside the following dictionary.
+
+my_dict = {"values_1":{"v1":3,"v2":6},"points":{"points1":9,"points2":[10,300,15]}}
+print(my_dict["points"]['points2'][1]) #Use dictionary indices to extract the second item of points2
