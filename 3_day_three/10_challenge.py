ebe431b6cc7f7bd3aa7686e31334f6b4895d94c3
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-01 21:14:39 -0300

    Update 10_challenge.py

--- 3_day_three/10_challenge.py
+++ 3_day_three/10_challenge.py
@@ -1,19 +1,23 @@
 # type any text, a poem or anyone
-from numpy.ma.core import append
-
-text = input("type any text, a poem or anyone: ")
+text = "Jesus is the salt of the earth and the light of the world."
+#text = input("type any text, a poem or anyone: ")
 list_counted_out = []
 
 # ask the user three letters
 # TODO: store those letters in a list
-letters = input("type three letters, with spaces: ")
+#letters = input("type three letters, with spaces: ")
+letters = "a i J".lower()
 letters_list = letters.split(" ")
 
 # 1 - how many times appears each of those letters
 # TODO: pass text and letters to lowercase
 for x in letters_list:
-    list_counted_out = append(x)
-    print(list_counted_out)
+    if x in text:
+        list_counted_out.append(x)
+
+    else:
+        pass
+print(list_counted_out)
 
 # 2 - how many words in total
 
