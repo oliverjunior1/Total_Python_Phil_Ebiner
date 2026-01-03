cb03aa49000a1e45bab165be9c66a2e89d8ad544
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-02 21:16:39 -0300

    Create 2_logical_operators.py

---
+++ 4_day_four/2_logical_operators.py
@@ -0,0 +1,19 @@
+my_bool = 4 < 5 < 6
+print(my_bool)
+
+my_bool = (55 == 55) and (5 == 2 + 3)
+print(my_bool)
+
+text = "This sentence is short"
+
+my_bool = ('sentence' in text) and ('python' in text)
+print(my_bool)
+
+# double negation is true
+my_bool = not 'a' != 'a'
+print(my_bool)
+
+
+
+
+
