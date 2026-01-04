2b9c2cf8db0e69901c11f1885929d5da47d1c7ec
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-03 23:47:18 -0300

    Create 4_looping_for.py

---
+++ 4_day_four/4_looping_for.py
@@ -0,0 +1,11 @@
+# Given the following list of numbers, calculate the sum of all the
+# numbers using For loops and store the result of the sum in a variable
+# called
+
+list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+
+sum_numbers = 0
+
+for x in list_numbers:
+    sum_numbers += x
+print(sum_numbers)
