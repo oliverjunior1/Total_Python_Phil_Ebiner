130b39057c3e5f2013ded10bd637ad2865946125
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-06 20:17:55 -0300

    Delete 3_looping_for.py

--- 4_day_four/3_looping_for.py
+++
@@ -1,7 +0,0 @@
-# Using For loops, greet all members of a class, printing "Hello" + their name.
-#
-# For example: "Hello Norville"
-
-students = ["Norville", "Fred", "Velma", "Daphne"]
-for x in students:
-    print(f"Hello {x}")
