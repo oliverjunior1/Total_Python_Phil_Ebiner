65b25b289432dc8f3ed2e1aaf6c3a2b867e02083
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-06 20:18:22 -0300

    Create 20_range.py

---
+++ exercises/20_range.py
@@ -0,0 +1,8 @@
+# Create a list consisting of all the numbers from 2500 to 2585
+# (inclusive). Store this list in the variable my_list.
+my_list = []
+
+for x in range(2500,2586):
+    my_list.append(x)
+
+print(my_list)
