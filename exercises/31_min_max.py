6d8d67ee36508b9e9ee0eda6e5a4847e09ef0655
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-10 18:40:03 -0300

    Create 31_min_max.py

---
+++ exercises/31_min_max.py
@@ -0,0 +1,7 @@
+dictionary_ages = {"Tony":55, "Paulie":42, "Meadow":78, "Vito":44, "Ralph":24, "Sarah":35, "Evan":19, "Jean":2 ,"Ned":49}
+
+minimum_age = min(dictionary_ages.values())
+last_name = dictionary_ages[minimum_age]
+
+print(minimum_age)
+print(last_name)
