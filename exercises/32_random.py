330252c5c0d39e6e787c55da590dfc9cd48200c5
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-11 17:35:54 -0300

    Create 32_random.py

---
+++ exercises/32_random.py
@@ -0,0 +1,9 @@
+# Implement the randint() function from the random library that
+# allows you to obtain an integer from 1 to 10, and store that value
+# in a variable called random_number.
+import random
+
+random_number = random.randint(1,10)
+print(random_number)
+
+
