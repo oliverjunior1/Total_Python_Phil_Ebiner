b11dbbbc039330e35b7c64018864c76251f02fff
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-12 22:00:22 -0300

    Update 32_random.py

--- exercises/32_random.py
+++ exercises/32_random.py
@@ -1,9 +1,6 @@
-# Implement the randint() function from the random library that
-# allows you to obtain an integer from 1 to 10, and store that value
-# in a variable called random_number.
-import random
+# Implement the random() function from the random library to obtain a
+# real number between 0 and 1, and store that value in a variable called
+# random_number.
 
-random_number = random.randint(1,10)
-print(random_number)
 
 
