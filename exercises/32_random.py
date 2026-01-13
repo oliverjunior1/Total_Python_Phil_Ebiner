9673fc9710db6cd686ae5847913071ade8e3313c
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-12 22:01:38 -0300

    Update 32_random.py

--- exercises/32_random.py
+++ exercises/32_random.py
@@ -1,6 +1,10 @@
 # Implement the random() function from the random library to obtain a
 # real number between 0 and 1, and store that value in a variable called
 # random_number.
+import random
 
+random_number = random.random()
+
+print(random_number)
 
 
