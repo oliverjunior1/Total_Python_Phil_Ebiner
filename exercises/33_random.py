5a4995ad4b536935cf5af6560923627645ecab8f
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-13 21:28:58 -0300

    Create 33_random.py

---
+++ exercises/33_random.py
@@ -0,0 +1,11 @@
+# Use the random library's choice() method to get a random item from
+# the list of names below, and store the chosen name in a variable called
+# raffle.
+import random
+
+names = ["Samantha", "Carrie", "Chris", "Charlotte", "Richard"]
+raffle = random.choice(names)
+
+print(raffle)
+
+
