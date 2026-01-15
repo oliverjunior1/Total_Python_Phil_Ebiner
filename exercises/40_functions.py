060a26719720d8f7eef4c42108e02533d34dce8f
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-15 20:39:45 -0300

    Create 40_functions.py

---
+++ exercises/40_functions.py
@@ -0,0 +1,7 @@
+# Declare a function called greet, which every time it is called prints "Hello world!"
+#
+# You should only define the function, you should not call it later.
+
+def greet():
+    print("Hello world!")
+
