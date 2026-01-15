c5f812b029bb30effc8f42554447b8061e3b79ff
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-15 20:18:11 -0300

    Create 38_help.py

---
+++ exercises/38_help.py
@@ -0,0 +1,11 @@
+# Add the element "orange" as the fourth element of the following list fruits, using the insert() method:
+#
+# fruits = ["mango", "banana", "cherry", "plum", "grapefruit"]
+#
+# Search the documentation for the requested method to know how it works.
+
+fruits = ["mango", "banana", "cherry", "plum", "grapefruit"]
+
+fruits.insert(3,'orange')
+
+print(fruits)
