8084b8608d3dbb1aa4dac1860fe809d72f85d739
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-15 20:46:12 -0300

    Create 41_functions.py

---
+++ exercises/41_functions.py
@@ -0,0 +1,9 @@
+# Declare a Function: Create a function called welcome that takes a person's name as an argument. This function should print "Welcome {name}!" whenever it is called.
+def welcome(name):
+    print(f"Welcome {name}!")
+
+# Create a Variable: Define a variable named name and store any name of your choice in it.
+name = "Joaquim"
+# Note: Only define the function and create the variable. Do not call the function afterwards.
+
+
