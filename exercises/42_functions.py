40a726cbde1b4839a6879d7b852e9e30fec5d70c
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-15 21:00:41 -0300

    Create 42_functions.py

---
+++ exercises/42_functions.py
@@ -0,0 +1,10 @@
+# Declare a function called square, that takes any number as an argument, and each time it is called, it prints the square of that
+# number on the screen (that is, the value to the second power).
+def square(number):
+    print(number**2)
+
+# The name of the argument that this function must take is number. Create this variable and assign it any number.
+number = 9
+# You should only define the function and create the variable, you should not call the function afterwards.
+
+
