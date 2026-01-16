a36f569de8e4d60a1e9b273e52ab9624a7c27c8c
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-15 21:15:20 -0300

    Create 43_functions_return.py

---
+++ exercises/43_functions_return.py
@@ -0,0 +1,4 @@
+# Create a function called power that takes two numeric values as arguments. It must return the number that results
+# from solving a power, using the first number as the base, and the second as the exponent:
+def power(first_number, second_number):
+    return first_number**second_number
