09655921b1278adb44948f87da08928aa55d28ec
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-17 18:22:00 -0300

    Create 46_dynamic_function.py

---
+++ exercises/46_dynamic_function.py
@@ -0,0 +1,6 @@
+# Create a function (all_positives) that returns True if all the values in a list are positive, and False if at least
+# one of the values is negative. Create a list named numbers with positive and negative values.
+#
+# Don't call the function, you just need to define it.
+def all_positives():
+    pass
