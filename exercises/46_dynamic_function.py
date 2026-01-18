d7f54e36f738cfb799a85c50eb0ad1ee602b8688
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-18 16:15:48 -0300

    Update 46_dynamic_function.py

--- exercises/46_dynamic_function.py
+++ exercises/46_dynamic_function.py
@@ -2,5 +2,8 @@
 # one of the values is negative. Create a list named numbers with positive and negative values.
 #
 # Don't call the function, you just need to define it.
-def all_positives():
-    pass
+def all_positives(numbers):
+    return numbers in range(-10000,0)
+
+numbers = [10,15,20,18]
+print(all_positives(numbers))
