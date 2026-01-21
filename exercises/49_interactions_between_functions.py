b72e2bd6102c64837e2372a94f243932f4f1fac8
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-21 20:58:59 -0300

    Create 49_interactions_between_functions.py

---
+++ exercises/49_interactions_between_functions.py
@@ -0,0 +1,23 @@
+# Create a function (throw_dice) that "throws" two random dice and
+# returns its results (the function MUST RETURN TWO VALUES AS A RESULT,
+# both of which must be between 1 and 6, randomly).
+def throw_dice():
+    th
+
+# Pass the result of these two dice to a function called roll_result (meaning that this second function MUST RECEIVE TWO ARGUMENTS) and return -without printing it- a certain message according to the what the sum of these values results:
+#
+# If the sum is less than or equal to 6:
+#
+# "The sum of your dice is {sum_dice}. Unfortunate"
+#
+# If the sum is greater than 6 and less than 10:
+#
+# "The sum of your dice is {sum_dice}. You have a good chance"
+#
+# If the sum is greater than or equal to 10:
+#
+# "The sum of your dice is {sum_dice}. It looks like a winning roll"
+#
+# Hint: use the random library's choice or randint method to choose a
+# random value between 1 and 6.
+
