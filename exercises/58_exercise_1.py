9a7fc06b1c6ef197f4bdac797cd14ad56425a381
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-27 20:28:03 -0300

    Update 58_exercise_1.py

--- exercises/58_exercise_1.py
+++ exercises/58_exercise_1.py
@@ -3,25 +3,7 @@
 # If the sum of the 3 numbers is less than 10, it must return the lowest number. If the sum of the 3 numbers is a value
 # between 10 and 15 (included), then it must return the number with the intermediate value.
 
-def return_distincts(*args):
-    sum = 0
-    list_min_max = []
-    list_2 = []
-    for arg in args:
-        list_min_max.append(arg)
-        list_2 = sorted(list_min_max)
-        sum += arg
-        if sum > 15:
-            return list_2[-1]
-        elif 15 >= sum > 10:
-            return list_2[1]
-        else:
-            return list_2[0]
-
-num1 = int(input("Put the number one: "))
-num2 = int(input("Put the number two: "))
-num3 = int(input("Put the number three: "))
-
-print(return_distincts(num1, num2, num3))
+def return_distincts():
+    pass
 
 
