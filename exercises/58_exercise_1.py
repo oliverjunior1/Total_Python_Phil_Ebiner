34d3a001ffaa2818d9c08a5ca8c021c2a290092c
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-27 21:38:19 -0300

    Update 58_exercise_1.py

--- exercises/58_exercise_1.py
+++ exercises/58_exercise_1.py
@@ -2,8 +2,37 @@
 # greater than 15, it must return the highest number.
 # If the sum of the 3 numbers is less than 10, it must return the lowest number. If the sum of the 3 numbers is a value
 # between 10 and 15 (included), then it must return the number with the intermediate value.
+def return_disticts():
+    num1 = int(input("Put the fist number: "))
+    num2 = int(input("Put the second number: "))
+    num3 = int(input("Put the third number: "))
+    sum_numbers = num1 + num2 + num3
+    if sum_numbers>15:
+        if num1>num2 and num1>num3:
+            print(num1)
+        elif num2>num1 and num2>num3:
+            print(num2)
+        else:
+            print(num3)
+    elif sum_numbers <= 15 and sum_numbers>10:
+        if num1<num2 and num1>num3:
+            print(num1)
+        elif num2<num1 and num2>num3:
+            print(num2)
+        else:
+            print(num3)
+    else:
+        if num1<num2 and num1<num3:
+            print(num1)
+        elif num2<num1 and num2<num3:
+            print(num2)
+        else:
+            print(num3)
 
-def return_distincts():
-    pass
+return_disticts()
+
+
+
+
 
 
