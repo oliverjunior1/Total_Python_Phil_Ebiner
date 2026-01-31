35ca8daa10ce2cc97050b5606c3a89931071fefa
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-31 12:47:47 -0300

    Create 60_exercise_2.py

---
+++ exercises/60_exercise_2.py
@@ -0,0 +1,17 @@
+# Write a function (you can name it whatever you want) that takes any word as a parameter, and returns
+# and returns all of its unique letters (without repetition) in alphabetical order.
+# For example, if when calling this function we pass the word 'entertaining', it should return
+# ['a', 'e', 'g', 'i', 'n', 'r', 't']
+
+type_a_phrase = input("Type any phrase: ")
+list_phrase = []
+
+for x in type_a_phrase:
+    list_phrase.append(x)
+
+print(list_phrase)
+
+
+
+
+
