a53f44dcce5be1b4a1964f522e30369bc90d1ca8
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-31 16:16:44 -0300

    Update 61_exercise_3.py

--- exercises/61_exercise_3.py
+++ exercises/61_exercise_3.py
@@ -6,3 +6,11 @@
 # (5,6,1,0,0,9,3,5)>>>True
 # (6,0,5,1,0,3,0,1)>>>False
 
+def has_double_zero(*args):
+    for i in range(len(args) - 1):
+        if args[i] == 0 and args[i + 1] == 0:
+            return True
+    return False
+
+print(has_double_zero(5,6,1,0,0,9,3,5))
+print(has_double_zero(6,0,5,1,0,3,0,1))
